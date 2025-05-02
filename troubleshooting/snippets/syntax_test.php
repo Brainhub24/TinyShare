cb001bfe16aa5aef1452fdac42f79b93ca7deab4
­Jason Prime <600613.m6@gmail.com>
2025-05-02 13:05:45 +0200

    Update syntax_test.php
    Note for my Son

--- troubleshooting/snippets/syntax_test.php
+++ troubleshooting/snippets/syntax_test.php
@@ -1,4 +1,8 @@
 <?php
+/**
+    This has written one of my son, they need to understand the concept and syntax of PHP!
+    Good job! :*
+**/
 echo "Testing syntax...";
 if (true) {
     echo "OK!";
